e41f977d50d9bfccd583351a04462fa791537b1d
Jack <jack.saville@hmcts.net>
2023-05-22 12:34:39 +0100

    fixed main branch conflicts

--- src/gatling/simulations/uk/gov/hmcts/reform/LandA/performance/scenarios/LAUScenario.scala
+++ src/gatling/simulations/uk/gov/hmcts/reform/LandA/performance/scenarios/LAUScenario.scala
@@ -28,11 +28,11 @@ object LAUScenario {
         .check(substring("Sign in"))
         .check(css("input[name='_csrf']", "value").saveAs("csrfToken")))
     }
-    .pause(ThinkTime)
+      .pause(ThinkTime)
 
   val LAULogin =
 
-    doSwitch("${env}") (
+    doSwitch("#{env}") (
       "perftest" -> feed(Users),
       "aat" -> feed(UsersAat)
     )
@@ -40,11 +40,11 @@ object LAUScenario {
         exec(http("LAU Login")
           .post(IdamURL + "/login?client_id=lau&response_type=code&redirect_uri=" + BaseURL + "/oauth2/callback")
           .headers(CommonHeader.navigation_headers)
-          .formParam("username", "${email}")
-          .formParam("password", "${password}")
+          .formParam("username", "#{email}")
+          .formParam("password", "#{password}")
           .formParam("save", "Sign in")
           .formParam("selfRegistrationEnabled", "false")
-          .formParam("_csrf", "${csrfToken}")
+          .formParam("_csrf", "#{csrfToken}")
           .check(substring("Case Audit Search")))
       }
       .pause(ThinkTime)
@@ -52,7 +52,7 @@ object LAUScenario {
   //Perform a case audit search and download the CSV file
   val LAUCaseAuditSearch =
 
-    doSwitch("${env}") (
+    doSwitch("#{env}") (
       "perftest" -> feed(CaseAuditSearches),
       "aat" -> feed(CaseAuditSearchesAat)
     )
@@ -60,12 +60,12 @@ object LAUScenario {
         exec(http("LAU Case Audit Search")
           .post(BaseURL + "/case-search")
           .headers(CommonHeader.navigation_headers)
-          .formParam("userId", "${userID}")
+          .formParam("userId", "#{userID}")
           .formParam("caseRef", "")
-          .formParam("startTimestamp", "${caseStartTimestamp}")
+          .formParam("startTimestamp", "#{caseStartTimestamp}")
           .formParam("caseTypeId", "")
-          .formParam("caseJurisdictionId", "${caseJurisdictionId}")
-          .formParam("endTimestamp", "${caseEndTimestamp}")
+          .formParam("caseJurisdictionId", "#{caseJurisdictionId}")
+          .formParam("endTimestamp", "#{caseEndTimestamp}")
           .formParam("page", "1")
           .check(substring("Case Activity Results"))
           .check(regex("""Case Activity Results</li>(?s)\s*?<p class="govuk-body">No results found""").optional.saveAs("noCaseResults"))
@@ -73,46 +73,46 @@ object LAUScenario {
       }
       .pause(ThinkTime)
 
-    //only continue if results were found ('No results found' wasn't found on the results page)
-    .doIf("${noCaseResults.isUndefined()}") {
+      //only continue if results were found ('No results found' wasn't found on the results page)
+      .doIf("#{noCaseResults.isUndefined()}") {
 
-      //only load the second page if there are more pages available
-      doIf("${moreCasePages.exists()}") {
+        //only load the second page if there are more pages available
+        doIf("#{moreCasePages.exists()}") {
 
-        group("LAU_050_CaseAuditPage2") {
-          exec(http("LAU Case Audit Page 2")
-            .get(BaseURL + "/case-activity/page/2")
-            .headers(CommonHeader.navigation_headers)
-            .check(substring("Page 2")))
+          group("LAU_050_CaseAuditPage2") {
+            exec(http("LAU Case Audit Page 2")
+              .get(BaseURL + "/case-activity/page/2")
+              .headers(CommonHeader.navigation_headers)
+              .check(substring("Page 2")))
+          }
+            .pause(ThinkTime)
+
         }
-        .pause(ThinkTime)
+
+          .group("LAU_060_CaseActivityDownload") {
+            exec(http("Case Activity CSV Download")
+              .get(BaseURL + "/case-activity/csv")
+              .headers(CommonHeader.download_headers)
+              .check(substring("Case Jurisdiction Id"))
+              .check(substring("filename")))
+          }
+          .pause(ThinkTime)
+
+          .group("LAU_070_CaseSearchDownload") {
+            exec(http("Case Search CSV Download")
+              .get(BaseURL + "/case-searches/csv")
+              .headers(CommonHeader.download_headers)
+              .check(substring("Case Ref"))
+              .check(substring("filename")))
+          }
+          .pause(ThinkTime)
 
       }
-
-      .group("LAU_060_CaseActivityDownload") {
-        exec(http("Case Activity CSV Download")
-          .get(BaseURL + "/case-activity/csv")
-          .headers(CommonHeader.download_headers)
-          .check(substring("Case Jurisdiction Id"))
-          .check(substring("filename")))
-      }
-      .pause(ThinkTime)
-
-      .group("LAU_070_CaseSearchDownload") {
-        exec(http("Case Search CSV Download")
-          .get(BaseURL + "/case-searches/csv")
-          .headers(CommonHeader.download_headers)
-          .check(substring("Case Ref"))
-          .check(substring("filename")))
-      }
-      .pause(ThinkTime)
-
-    }
 
   //Perform a logon audit search and download the CSV file
   val LogonsAuditSearch =
 
-    doSwitch("${env}") (
+    doSwitch("#{env}") (
       "perftest" -> feed(LogonAuditSearches),
       "aat" -> feed(LogonAuditSearchesAat)
     )
@@ -121,9 +121,9 @@ object LAUScenario {
           .post(BaseURL + "/logon-search")
           .headers(CommonHeader.navigation_headers)
           .formParam("userId", "")
-          .formParam("emailAddress", "${logonEmailAddress}")
-          .formParam("startTimestamp", "${logonStartTimestamp}")
-          .formParam("endTimestamp", "${logonEndTimestamp}")
+          .formParam("emailAddress", "#{logonEmailAddress}")
+          .formParam("startTimestamp", "#{logonStartTimestamp}")
+          .formParam("endTimestamp", "#{logonEndTimestamp}")
           .formParam("page", "1")
           .check(regex("Logons Audit Results|System Logon Results"))
           .check(regex("""System Logon Results</h2>(?s)\s*?<p class="govuk-body">No results found""").optional.saveAs("noLogonResults"))
@@ -131,33 +131,33 @@ object LAUScenario {
       }
       .pause(Environment.thinkTime)
 
-    //only continue if results were found ('No results found' wasn't found on the results page)
-    .doIf("${noLogonResults.isUndefined()}") {
+      //only continue if results were found ('No results found' wasn't found on the results page)
+      .doIf("#{noLogonResults.isUndefined()}") {
 
-      //only load the second page if there are more pages available
-      doIf("${moreLogonPages.exists()}") {
+        //only load the second page if there are more pages available
+        doIf("#{moreLogonPages.exists()}") {
 
-        group("LAU_090_LogonAuditPage2") {
-          exec(http("LAU Logon Audit Page 2")
-            .get(BaseURL + "/logons/page/2")
-            .headers(CommonHeader.navigation_headers)
-            .check(substring("Page 2")))
+          group("LAU_090_LogonAuditPage2") {
+            exec(http("LAU Logon Audit Page 2")
+              .get(BaseURL + "/logons/page/2")
+              .headers(CommonHeader.navigation_headers)
+              .check(substring("Page 2")))
+          }
+            .pause(ThinkTime)
+
         }
-        .pause(ThinkTime)
+
+          .group("LAU_100_LogonActivityDownload") {
+            exec(http("Logon Activity CSV Download")
+              .get(BaseURL + "/logons/csv")
+              .headers(CommonHeader.download_headers)
+              .check(substring("Ip Address"))
+              .check(substring("filename")))
+
+          }
+          .pause(Environment.thinkTime)
 
       }
-
-      .group("LAU_100_LogonActivityDownload") {
-        exec(http("Logon Activity CSV Download")
-          .get(BaseURL + "/logons/csv")
-          .headers(CommonHeader.download_headers)
-          .check(substring("Ip Address"))
-          .check(substring("filename")))
-
-      }
-      .pause(Environment.thinkTime)
-
-    }
 
 }
 
